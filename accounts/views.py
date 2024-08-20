24b2723b985d7e8c68bf0a3d08cfb9a0a25506c3
Robertin76 <robertnobrega14@gmail.com>
2024-08-20 18:49:38 -0300

    terceito commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import *
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(requests):
-    products = Product.object.all()
+    products = Product.objects.all()
     return render(requests, 'accounts/products.html', {'products': products})
 
-def customer(requests):
-    return render(requests, 'accounts/customer.html')
+def customer(requests, pk_test):
+    customer = Customer.objects.get(id=pk_test)
 
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+
+    context = {'customer':customer, 'orders':orders, 'orders_count':orders_count}
+    return render(requests, 'accounts/customer.html', context)
