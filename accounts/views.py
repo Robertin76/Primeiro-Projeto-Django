a0bb5c480267ea0cad409cd54effaa313a72de86
Robertin76 <robertnobrega14@gmail.com>
2024-08-19 21:01:28 -0300

    primeiro commit

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def products(requests):
+    return render(requests, 'accounts/products.html')
+
+def customer(requests):
+    return render(requests, 'accounts/customer.html')
+
