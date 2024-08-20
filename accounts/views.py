deb06d9c96070962372980d87d9d6cc80bf06784
Robertin76 <robertnobrega14@gmail.com>
2024-08-20 10:22:17 -0300

    segundo commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import *
 
 # Create your views here.
 
@@ -7,7 +8,8 @@ def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def products(requests):
-    return render(requests, 'accounts/products.html')
+    products = Product.object.all()
+    return render(requests, 'accounts/products.html', {'products': products})
 
 def customer(requests):
     return render(requests, 'accounts/customer.html')
